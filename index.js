ffebeb1bd248831eec881edb2b82dfa17ce6ad1a
Mac Rowe <m.rowe42@gmail.com>
2021-05-24 15:24:36 -0400

    commit tree 303cc6d9e18242df2a265ef028714e0bd1ac266b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, adj) {
+    const array = []
+    for (let i = 0; i < names.length; i++ ) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${adj} gift!`);
+    }
+    return array;
+};
+
+function countDown(i) {
+    for (let i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+}
